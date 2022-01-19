92ff3aa998c4c2b19be72d29c3ea3a8786e47413
Thomas Weichselbaumer <kontakt@themecoder.de>
2022-01-19 17:30:35 +0100

    updated Scroll to Top button script to use vanilla js

--- includes/modules/class-scroll-to-top.php
+++ includes/modules/class-scroll-to-top.php
@@ -49,7 +49,7 @@ class Mercia_Pro_Scroll_To_Top {
 		// Call Credit Link function of theme if credit link is activated.
 		if ( true === $theme_options['scroll_to_top'] && ! self::is_amp() ) :
 
-			wp_enqueue_script( 'mercia-pro-scroll-to-top', MERCIA_PRO_PLUGIN_URL . 'assets/js/scroll-to-top.min.js', array( 'jquery' ), '20210309', true );
+			wp_enqueue_script( 'mercia-pro-scroll-to-top', MERCIA_PRO_PLUGIN_URL . 'assets/js/scroll-to-top.min.js', array(), '20220119', true );
 
 			// Passing Parameters to navigation.js.
 			wp_localize_script( 'mercia-pro-scroll-to-top', 'merciaProScrollToTop', array( 'icon' => mercia_get_svg( 'collapse' ) ) );
